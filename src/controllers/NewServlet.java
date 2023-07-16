740898df5e701ea27d1d296a7beb058ea3a706a8
zzban <zzban@STORM-PC>
2023-07-16 09:38:57 +0900

    NewServletを作成

--- src/controllers/NewServlet.java
+++ src/controllers/NewServlet.java
@@ -1,10 +1,9 @@
 package controllers;
 
 import java.io.IOException;
-import java.util.List;
+import java.sql.Timestamp;
 
 import javax.persistence.EntityManager;
-import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -15,35 +14,45 @@ import models.Task;
 import utils.DBUtil;
 
 /**
- * Servlet implementation class IndexServlet
+ * Servlet implementation class NewServlet
  */
-@WebServlet("/index")
-public class IndexServlet extends HttpServlet {
+@WebServlet("/new")
+public class NewServlet extends HttpServlet {
     private static final long serialVersionUID = 1L;
 
     /**
      * @see HttpServlet#HttpServlet()
      */
-    public IndexServlet() {
+    public NewServlet() {
         super();
     }
 
     /**
-     * @param Task
      * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
      */
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         EntityManager em = DBUtil.createEntityManager();
+        em.getTransaction().begin();
 
-        List<Task> Task = em.createNamedQuery("getAllTasks", Task.class).getResultList();
+        Task Task = new Task();
+
+        String title = "kaiji";
+        Task.setTitle(title);
+
+        String content = "hello";
+        Task.setContent(content);
+
+        Timestamp currentTime = new Timestamp(System.currentTimeMillis());
+        Task.setCreated_at(currentTime);
+        Task.setUpdated_at(currentTime);
+
+        em.persist(Task);
+        em.getTransaction().commit();
+
+        response.getWriter().append(Integer.valueOf(Task.getId()).toString());
 
         em.close();
-
-        request.setAttribute("tasks", Task);
-
-        RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/views/tasks/index.jsp");
-        rd.forward(request, response);
     }
 
 }
